c76c3b2daec91d5ad2eddfbcfcfb0690025241dc
JunSeong <ebseud6135@naver.com>
2021-04-27 20:10:34 +0900

    feat: Spring Web Mvc 예제 추가

--- src/main/java/net/skhu/hello/controller/HomeController.java
+++ src/main/java/net/skhu/hello/controller/HomeController.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
+
 @Controller
 public class HomeController {
 
-    @GetMapping("home/index")
+    @GetMapping("/home/index")
     public String index(Model model) {
         model.addAttribute("message", "좋은 아침 입니다.");
+        model.addAttribute("now", new Date());
         return "home/index";
+    }
+
+    @GetMapping("/home/test1")
+    public String test1(Model model) {
+        model.addAttribute("myName", "DunDung");
+        return "home/test1";
     }
 }
--- src/main/webapp/WEB-INF/views/home/index.jsp
+++ src/main/webapp/WEB-INF/views/home/index.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,5 +8,7 @@
 <body>
 <h1>안녕하세요</h1>
 <h3>${ message }</h3>
+<h3><fmt:formatDate pattern = "yyyy-MM-dd HH:mm:ss" value = "${ now }" /></h3>
 </body>
 </html>
+
